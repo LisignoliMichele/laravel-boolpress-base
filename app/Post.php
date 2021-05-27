fae572d419abd1369dd156f05c633287ea90e578
Michele Lisignoli <michele@lisignoli.com>
2021-05-27 17:12:29 +0200

    created the Models and the relation between them

--- app/Post.php
+++ app/Post.php
@@ -7,9 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $guarded = [];
-    
     public function comments()
     {
         return $this->hasMany('App\Comment');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
     }
 }
--- database/migrations/2021_05_27_150038_create_tags_table.php
+++ database/migrations/2021_05_27_150038_create_tags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersinfoTable extends Migration
+class CreateTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,12 @@ class CreateUsersinfoTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->unique();
+            $table->string('slug', 50)->unique();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +28,6 @@ class CreateUsersinfoTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('tags');
     }
 }
